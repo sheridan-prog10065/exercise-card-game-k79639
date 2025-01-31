debdbc81ee2d36796760a60bab527dedac33a17a
Karan <k79639@gmail.com>
2025-01-31 14:51:32 -0500

    Defined classes, enum, struct and field variables

---
+++ CardGame_Interactive/CardGameInteractive/CardDeck.cs
@@ -0,0 +1,11 @@
+namespace CardGameInteractive;
+/// <summary>
+/// Defines the card deck as the list of cards
+/// </summary>
+public class CardDeck
+{
+    /// <summary>
+    /// The list of cards in the deck
+    /// </summary>
+    private List<Card> _cardList;
+}
---
+++ CardGame_Interactive/CardGameInteractive/Score.cs
@@ -0,0 +1,16 @@
+namespace CardGameInteractive;
+/// <summary>
+/// Defines the score of the game
+/// </summary>
+public struct Score
+{
+    /// <summary>
+    /// The number of rounds won by the player of the game
+    /// </summary>
+    private int _playerScore;
+    
+    /// <summary>
+    /// The number of rounds won by the house
+    /// </summary>
+    private int _houseScore;
+}
